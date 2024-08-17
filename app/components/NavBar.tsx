a72f8c2e159cf7cb44519aeda10123a5fe00e468
achmadky <achmad.dhikrillah@gmail.com>
2024-08-18 02:19:37 +0700

    change color to grey

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -5,23 +5,25 @@ import Link from 'next/link';
 
 const NavBar = () => {
   return (
-    <nav className="bg-neutral-200 dark:bg-neutral-800 border-b border-neutral-300 dark:border-neutral-700 fixed top-0 left-0 w-full z-20">
+    <nav className="bg-neutral-100 dark:bg-neutral-800 border-b border-neutral-300 dark:border-neutral-700 shadow-md fixed top-0 left-0 w-full z-20 rounded-b-lg">
       <div className="container mx-auto p-4 flex justify-between items-center">
-        <ul className="flex space-x-4">
+        <ul className="flex space-x-6">
           <li>
             <Link
               href="/food-info"
-              className="text-neutral-700 dark:text-neutral-300 hover:text-neutral-900 dark:hover:text-white text-lg font-semibold"
+              className="relative group text-neutral-700 dark:text-neutral-300 hover:text-neutral-900 dark:hover:text-white"
             >
-              Food Info
+              <span className="text-lg font-semibold">Food Info</span>
+              <span className="absolute left-0 -bottom-1 w-full h-1 bg-neutral-900 dark:bg-neutral-200 rounded-full transform scale-x-0 group-hover:scale-x-100 transition-transform"></span>
             </Link>
           </li>
           <li>
             <Link
               href="/calorie-tracker"
-              className="text-neutral-700 dark:text-neutral-300 hover:text-neutral-900 dark:hover:text-white text-lg font-semibold"
+              className="relative group text-neutral-700 dark:text-neutral-300 hover:text-neutral-900 dark:hover:text-white"
             >
-              Calorie Tracker
+              <span className="text-lg font-semibold">Calorie Tracker</span>
+              <span className="absolute left-0 -bottom-1 w-full h-1 bg-neutral-900 dark:bg-neutral-200 rounded-full transform scale-x-0 group-hover:scale-x-100 transition-transform"></span>
             </Link>
           </li>
         </ul>
